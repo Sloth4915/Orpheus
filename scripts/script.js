c462dc9322cb4574b88bb0c54f786a5b4c526cd8
Sloth4915 <148401810+Sloth4915@users.noreply.github.com>
2026-04-09 14:45:57 -0700

    Fix the syntax error from previous fix 💀

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ const storageKeys = {
 const MISSING_LOGO = "https://frc-cdn.firstinspires.org/eventweb_frc/ProgramLogos/FIRSTicon_RGB_withTM.png"
 
 const toolName = "Orpheus"
-const version = "2.5.5"
+const version = "2.5.6"
 
 let eventKey
 let event_data
